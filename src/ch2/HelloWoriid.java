28bc13c3bf0512e50069842e344bc5218acf1e93
changzune <changzunelee@gmail.com>
2021-09-30 15:42:54 +0900

    커밋

---
+++ src/ch2/HelloWoriid.java
@@ -0,0 +1,12 @@
+package ch2;
+
+public class HelloWoriid {
+
+	public static void main(String[] args) {
+		System.out.println("hellow World!");
+		System.out.println("자바프로그래밍 입니다.");
+		System.out.println("자바프로그래밍의 입니다.");
+		System.out.println("안녕하세요");
+		}
+
+}
