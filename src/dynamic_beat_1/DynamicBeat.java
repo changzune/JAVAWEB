bd2c63864b35ae314f3d68e7d6ee894f03eb84af
changzune <changzunelee@gmail.com>
2021-10-02 19:48:30 +0900

    리듬게임

---
+++ src/dynamic_beat_1/DynamicBeat.java
@@ -0,0 +1,19 @@
+package dynamic_beat_1;
+
+import javax.swing.JFrame;
+
+public class DynamicBeat extends JFrame {
+	
+	public DynamicBeat( ) {
+		setTitle("Danamic beat");
+		setSize(Main.SCREEN_WIDTH,Main.SCREEN_HEIGT);
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+				
+	}
+	
+	
+
+}
