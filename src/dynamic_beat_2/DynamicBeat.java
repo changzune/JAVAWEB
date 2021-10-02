33d2839dc706187363425f7946f7c9e6c614b449
changzune <changzunelee@gmail.com>
2021-10-02 20:44:16 +0900

    일단 여기까찌

--- src/dynamic_beat_2/DynamicBeat.java
+++ src/dynamic_beat_2/DynamicBeat.java
@@ -1,4 +1,5 @@
 package dynamic_beat_2;
+
 import java.awt.Graphics;
 import java.awt.Image;
 
@@ -10,7 +11,7 @@ public class DynamicBeat extends JFrame {
 	private Image screenImage;
 	private Graphics screenGraphic;
 	
-	private Image image2;
+	private Image introBackground;
 	
 	
 	public DynamicBeat( ) {
@@ -20,11 +21,18 @@ public class DynamicBeat extends JFrame {
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
+	
 		
-		image2 = new ImageIcon(Main.class.getResource("../images/image2.jpg"));.getImage0;
-		
+		introBackground = new ImageIcon(Main.class.getResource("../imges/lmageBackground.jpg")).getImage();
 	}
 	
+	public void paint(Graphics g){
+		screenImage = createImage(Main.SCREEN_WIDTH, Main.SCREEN_HEIGT);
+		screenGraphic = screenImage.getGraphics();
+		screenDraw(screenGraphic);
+		g.drawImage(screenImage, 0, 0, null);
+		
+	}
 	
 
 }
