efa040634d668e625308dcace2b9844b8c298d04
changzune <changzunelee@gmail.com>
2021-10-05 23:23:52 +0900

    변수실습

--- src/ch3/C3.java
+++ src/ch3/C3.java
@@ -9,9 +9,21 @@ public class C3 {
 		int a123b;
 		int a123$;
 		int a123_$;
+
+		int b,c;
 		
+		a=3;
 		
+		c=a;
+		System.out.println(a);
+		a=a+4;
+		System.out.println(a);
 		
+		int num=0;
+		int totalCount=5;
+		
+		System.out.println(num);
+		System.out.println();
 
 	}
 
