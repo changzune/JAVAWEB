73a2f6875c7978920d601f3347a28c637a37ff04
changzune <changzunelee@gmail.com>
2021-10-09 20:02:25 +0900

    배열공부중

--- src/ex1/ArrayUtil.java
+++ src/ex1/ArrayUtil.java
@@ -8,10 +8,14 @@ public class ArrayUtil {
 		float average = 0f;
 		
 		for(int i=0; i<num.length; i++) {
-			
+			total+=num[i];
 			
 		}
 		
+		average=(float)total/num.length;
+		
+		System.out.println("¹è¿­ÀÉ ÃÑÇÕ " + total + ",¹è¿­ÀÇ Æò±Õ" + average);
+	
 		
 		
 	}
