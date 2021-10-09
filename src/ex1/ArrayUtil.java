1ae61a5421a3494fef0650a122c1d31029b89928
changzune <changzunelee@gmail.com>
2021-10-09 20:10:57 +0900

    지역변수를 이용한 배열 만들기

--- src/ex1/ArrayUtil.java
+++ src/ex1/ArrayUtil.java
@@ -1,28 +1,43 @@
 package ex1;
 
 public class ArrayUtil {
-	
-	public void calcArray(int[] num) {
-		
-		int total = 0;
-		float average = 0f;
-		
-		for(int i=0; i<num.length; i++) {
-			total+=num[i];
-			
-		}
-		
-		average=(float)total/num.length;
-		
-		System.out.println("배열읠 총합 " + total + ",배열의 평균" + average);
-	
-		
-		
+	   //배열 요소값들의 총합과 평균을 구하는 메서드
+	   public void calcArray(int[] num){
+	      int total=0;  //총합을 저장하는 변수
+	      float average=0f; //평균을 저장하는 변수
+
+	      for(int i=0; i<num.length;i++){
+	         total+=num[i];
+	      }
+
+	      average=(float)total/num.length;
+	      System.out.println("배열의 총합:"+total+",배열의 평균:"+average);
+	   }
+	   
+	   //배열요소값을 오름차순으로 정렬하는 메서드
+	   public void sortArray(int[] num){
+	      int temp=0;
+
+	      for(int i=0; i<num.length;i++){
+	         for(int j=i+1; j<num.length;j++){
+	            if(num[i]>num[j]){
+	               temp=num[i];
+	               num[i]=num[j];
+	               num[j]=temp;
+	            }
+	         }
+	      } //end for
+
+	      for(int i=0; i<num.length;i++){
+	         System.out.print(num[i]+"\t");
+	      }
+	   }
 	}
-	
+
+
 	
 	
 	
 	
 
-}
+
