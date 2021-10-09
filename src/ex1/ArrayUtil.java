36a22be8cbe3503ecdf44fa80341a5f6bf5c81a1
changzune <changzunelee@gmail.com>
2021-10-09 19:53:14 +0900

    객체지향 배열 사용법 공부

---
+++ src/ex1/ArrayUtil.java
@@ -0,0 +1,24 @@
+package ex1;
+
+public class ArrayUtil {
+	
+	public void calcArray(int[] num) {
+		
+		int total = 0;
+		float average = 0f;
+		
+		for(int i=0; i<num.length; i++) {
+			
+			
+		}
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+
+}
