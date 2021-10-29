4375fa07b5bf6918803bb3b1e05f579ae348a149
changzune <changzunelee@gmail.com>
2021-10-29 21:38:12 +0900

    인터페이스 다형성

---
+++ src/java_jsk/FighterTest.java
@@ -0,0 +1,36 @@
+package java_jsk;
+
+abstract class Unit2 {
+	int x, y;
+
+	abstract void move(int x, int y);
+
+	void stop() {
+		System.out.println("멈춥니다.");
+
+	}
+
+}
+	interface Fightable {
+		void move(int x, int y); // public abstract가 생략됨
+		void attack(Fightable f); // public abstract가 생략됨
+	}
+
+class Fighter extends Unit2 implements Fightable{
+	public void move(int x, int y) { //오버라이딩 규칙: 조상보다 접근제어자가 좁으면 안된다.
+		System.out.println("["+x+","+y+"]로 이동");
+	}
+	public void attack(Fightable f) {
+		System.out.println(f+"를 공격");
+	}
+}
+
+public class FighterTest {
+
+	public static void main(String[] args) {
+		 Fighter f = new Fighter();
+		 f.move(100, 200);
+		 f.attack(new Fighter());
+	}
+
+}
