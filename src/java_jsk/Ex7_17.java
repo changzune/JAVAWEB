718409eebc4a34fbe28b4082d40e7f72168abe28
changzune <changzunelee@gmail.com>
2021-10-31 23:29:50 +0900

    주말공부

---
+++ src/java_jsk/Ex7_17.java
@@ -0,0 +1,9 @@
+package java_jsk;
+class Ex7_17 {
+	Object iv = new Object(){ void method(){} };         // 익명 클래스
+	static Object cv = new Object(){ void method(){} };  // 익명 클래스
+
+	void myMethod() {
+		Object lv = new Object(){ void method(){} };      // 익명 클래스
+	}
+}
