3156f05dbbeadebd1b3b3cc2de33068b10afd699
changzune <changzunelee@gmail.com>
2021-11-03 15:21:48 +0900

    홍팍자바

---
+++ src/hong/HeroTest.java
@@ -0,0 +1,37 @@
+package hong;
+
+public class HeroTest {
+	
+	  public static void main(String[] args) {
+		    // 두 싸움꾼 객체 생성
+		    Hero arthas = new Hero("아서스");
+		    Hero leona = new Hero("레오나");
+		    
+		    // 격투 시작
+		    Hero.battle(arthas, leona);
+		  }
+		}
+
+		class Hero {
+		  // 필드
+		  String name;
+		  int hp;
+		  
+		  // 생성자
+		  public Hero(String str) {
+		    name = str;
+		    hp = 30;
+		  }
+		  
+		  // 메소드(인스턴스 메소드)
+		  public void punch(Hero enemy) {
+		    /* 1. 해당 메소드를 완성하세요. */
+		  }
+		  
+		  // 메소드(클래스 메소드)
+		  public static void battle(Hero a, Hero b) {
+		    /* 2. 해당 메소드를 완성하시오. */
+		  }
+		}
+
+}
---
+++ src/hong/Hong22.java
@@ -0,0 +1,24 @@
+package hong;
+
+public class Hong22 {
+//아래 뼈대 코드를 동작의 분석하고, 이를 주석으로 작성하시오. 
+		  public static void main(String[] args) {
+		    //객체생성
+			Hero h1 = new Hero();
+		    //히어로 클래스에서 정의한 필드를 사용
+			//객체생성한것을 이용하여 객체값을 초기화;
+		    h1.name = "쥬리오";
+		    h1.hp = 100;
+		    h1.punch();
+		  }
+		}
+		class Hero { //클래스 정의
+		  // 필드에 이름과 생명을 넣어줌 펀치를 실행하면 이름과 펀치가 나가게됨 
+			//메인에서 실행된다.
+		  String name;
+		  int hp;
+		  // 메소드
+		  void punch() {
+		    System.out.println(name + "의 펀치!");
+		  }
+		}
