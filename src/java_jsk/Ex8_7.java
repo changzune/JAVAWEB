259079303a8db2b7b427d5e7eb02cd77957f0960
changzune <changzunelee@gmail.com>
2021-11-04 21:30:50 +0900

    자바의정석

---
+++ src/java_jsk/Ex8_7.java
@@ -0,0 +1,6 @@
+package java_jsk;
+class Ex8_7 {
+	public static void main(String[] args) {
+		throw new Exception();		// Exception을 고의로 발생시킨다.
+	}
+}
---
+++ src/java_jsk/Ex8_8.java
@@ -0,0 +1,6 @@
+package java_jsk;
+class Ex8_8 {
+	public static void main(String[] args) {
+		throw new RuntimeException();	// RuntimeException을 고의로 발생시킨다.
+	}
+}
