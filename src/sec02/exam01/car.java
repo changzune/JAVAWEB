36b086916707789efd6bfe287cf1227ce7567841
changzune <changzunelee@gmail.com>
2021-11-08 18:48:52 +0900

    혼공 필드

--- src/sec02/exam01/car.java
+++
@@ -1,7 +0,0 @@
-package sec02.exam01;
-
-public class car {
-	//Field
-	String campany ="현대자동차";
-
-}
---
+++ src/sec02/exam02/FieldInValueeExample.java
@@ -0,0 +1,14 @@
+package sec02.exam02;
+
+public class FieldInValueeExample {
+
+	public static void main(String[] args) {
+		FieldInValue fiv = new FieldInValue();
+		System.out.println(fiv.byteField);
+		System.out.println(fiv.shortField);
+		System.out.println(fiv.intField);
+		System.out.println(fiv.longFiled);
+
+	}
+
+}
