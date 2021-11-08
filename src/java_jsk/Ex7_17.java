8e14c710a21d7f943b788afe639c3acbc8cc908d
changzune <changzunelee@gmail.com>
2021-11-08 16:45:13 +0900

    혼자공부하는자바

--- src/java_jsk/Ex7_17.java
+++
@@ -1,9 +0,0 @@
-package java_jsk;
-class Ex7_17 {
-	Object iv = new Object(){ void method(){} };         // 익명 클래스
-	static Object cv = new Object(){ void method(){} };  // 익명 클래스
-
-	void myMethod() {
-		Object lv = new Object(){ void method(){} };      // 익명 클래스
-	}
-}
--- src/java_jsk/Ex8_8.java
+++
@@ -1,6 +0,0 @@
-package java_jsk;
-class Ex8_8 {
-	public static void main(String[] args) {
-		throw new RuntimeException();	// RuntimeException을 고의로 발생시킨다.
-	}
-}
