fb553ce37e0a2a96cb8a33d9c0e01cb81ebc55c4
changzune <changzunelee@gmail.com>
2021-11-09 22:40:03 +0900

    혼공 계산기

---
+++ src/sec04/exam/Calculater.java
@@ -0,0 +1,14 @@
+package sec04.exam;
+
+public class Calculater {
+	//Field
+	//Constructor
+	//Method
+	
+
+	void poworOn() {
+		System.out.println("전원을 켭니다.");
+		
+	}
+
+}
---
+++ src/sec04/exam/CalculatorExample.java
@@ -0,0 +1,11 @@
+package sec04.exam;
+
+public class CalculatorExample {
+
+	public static void main(String[] args) {
+		
+		Calculater myCalc = new Calculater();
+		myCalc.poworOn();
+	}
+
+}
