844f69a99e7484fbbe7ef3644a6304cab94b060e
changzune <changzunelee@gmail.com>
2021-11-10 05:54:17 +0900

    혼공 계산기

--- src/sec04/exam/CalculatorExample.java
+++ src/sec04/exam/CalculatorExample.java
@@ -6,6 +6,23 @@ public class CalculatorExample {
 		
 		Calculater myCalc = new Calculater();
 		myCalc.poworOn();
+		int result1 = myCalc.plus(5, 6);
+		System.out.println(result1);
+		
+		byte x = 10;
+		byte y = 4;
+		myCalc.divide(x, y);
+		
+		
+		double result2 = myCalc.divide(9, 4);
+		System.out.println(result2);
+		
+		myCalc.powerOff();
+		
+
 	}
+	
+	
+	
 
 }
