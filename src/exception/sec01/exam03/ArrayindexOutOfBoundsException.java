0e120d00234f6f44df69fe5f71cfc469603bf694
changzune <changzunelee@gmail.com>
2021-12-01 23:13:55 +0900

    commit

--- src/exception/sec01/exam03/ArrayindexOutOfBoundsException.java
+++ src/exception/sec01/exam03/ArrayindexOutOfBoundsException.java
@@ -1,18 +1,18 @@
-package exception.sec01.exam02;
+package exception.sec01.exam03;
 
 public class ArrayindexOutOfBoundsException {
 
 	public static void main(String[] args) {
-		
-		int[] arr = {1,2,3,};
-		
-		int result = arr[0] + arr[2];
-		
+		if(args.length ==2) {
+	
 		String data1 = args[0];
 		String data2 = args[1];
 		
 		System.out.println(data1);
 		System.out.println(data2);
+		} else {
+			System.out.println("두개의 매개값이 필요합니다.");
+		}
 		
 		
 
