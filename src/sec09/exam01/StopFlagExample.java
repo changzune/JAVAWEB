ae06043afa8f96fb041aacd987690dc79ffe9edc
changzune <changzunelee@gmail.com>
2021-12-17 23:14:12 +0900

    자바 api

---
+++ src/sec09/exam01/StopFlagExample.java
@@ -0,0 +1,16 @@
+package sec09.exam01;
+
+public class StopFlagExample {
+	public static void main(String[] args)  {
+		PrintThread printThread = new PrintThread();
+		printThread.start();
+		
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+		}
+		
+		printThread.setStop(true);
+	}
+}
+
